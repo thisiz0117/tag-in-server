0b360aaa08c007e5997c7680e0da2a0b4fc4f065
lanterva <lanterva@gmail.com>
2026-08-08 00:51:30 +0900

    fix: configModule 로드 순서 수정
    configModule 로드 전에 다른 모듈이 먼저 .env값을 가져올려는 시도로 인해 undifined가 리턴되는 문제가 있었음.
    이를 해결하기 위해 configModule이 먼저 로드되도록 함.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,34 @@
-import { RedisModule } from '@nestjs-modules/ioredis';
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
+import { RedisModule } from '@nestjs-modules/ioredis'
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AuthModule } from './auth/auth.module'
+import { ConfigModule } from '@nestjs/config'
+import { UserModule } from './user/user.module'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     RedisModule.forRoot({
       options: {
         host: 'localhost',
         port: 6379,
         db: 0,
-        lazyConnect: true
+        lazyConnect: true,
       },
-      type: 'single'
+      type: 'single',
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: process.env.IS_PRODUCTION === 'false' ? true : false
+      synchronize: process.env.IS_PRODUCTION === 'false' ? true : false,
     }),
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env'
-    }),
-    AuthModule
-    UserModule
+    AuthModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
